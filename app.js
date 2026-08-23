1a28f4ddc36d9a18d026df9e1afd2326facc9b42
Priyanshu Ojha <priyanshuoz.work@gmail.com>
2026-08-23 15:10:14 +0530

    Add validation to review section

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const engine = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
-const listingSchema = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review");
 
 let mongo_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -36,9 +36,19 @@ app.get("/", (req, res) => {
 });
 
 const validateListing = (req, res, next) => {
-  let {error} = listingSchema.validate(req.body);
+  let { error } = listingSchema.validate(req.body);
   if (error) {
-    let errMsg = error.details.map((el) => el.message).join(',')
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+const validateRevieew = (req, res, next) => {
+  let { error } = reviewSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, errMsg);
   } else {
     next();
@@ -74,7 +84,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("review");
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -108,17 +118,22 @@ app.delete(
   }),
 );
 
-app.post('/listings/:id/review', async(req, res) => {
-  let listing = await Listing.findById(req.params.id);
-  let newReview = new Review(req.body.review);
+//Review Route
+app.post(
+  "/listings/:id/reviews",
+  validateRevieew,
+  wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
 
-  listing.review.push(newReview);
+    listing.review.push(newReview);
 
-  await listing.save();
-  await newReview.save();
+    await listing.save();
+    await newReview.save();
 
-  res.redirect(`/listings/${listing._id}`)
-})
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
 
 // app.get('/testListing', async(req, res) => {
 //   let sampleList =new Listing({
--- schema.js
+++ schema.js
@@ -1,6 +1,7 @@
 const Joi = require("joi");
+const review = require("./models/review");
 
-module.exports = listingSchema = Joi.object({
+const listingSchema = Joi.object({
   listing: Joi.object({
     title: Joi.string().required(),
     description: Joi.string().required(),
@@ -13,3 +14,15 @@ module.exports = listingSchema = Joi.object({
     }).allow("", null),
   }).required(),
 });
+
+const reviewSchema = Joi.object({
+  review : Joi.object({
+    rating : Joi.number().required().min(1).max(5),
+    comment : Joi.string().required()
+  }).required(),
+})
+
+module.exports = {
+  listingSchema,
+  reviewSchema,
+};
